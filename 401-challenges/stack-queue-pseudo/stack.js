d3b67e1da07003d22ccc8f8176d7d2aa72971ece
Mohammed-Alramahi <vittosc1997@gmail.com>
2021-07-12 04:28:45 +0300

    testing github

---
+++ 401-challenges/stack-queue-pseudo/stack.js
@@ -0,0 +1,17 @@
+const Node = require("./node");
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+  push(value) {
+    let node = new Node(value);
+    node.next = this.top;
+    this.top = node;
+  }
+  pop() {
+    let tmp = this.top;
+    this.top = tmp.next;
+  }
+}
+module.exports = Stack;
